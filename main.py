e20d57d13155c86c95d66ac71361c2a19d1a9593
asturtz2 <alexstur@gmail.com>
2018-03-31 20:22:16 -0500

    Initial version of main

--- main.py
+++ main.py
@@ -9,27 +9,24 @@ import ptb_reader
 TIME_STEPS = 20
 BATCH_SIZE = 20
 
+tf.reset_default_graph()
 class PTBInput(object):
-      """The input data.
-  
-  Code sourced from https://github.com/tensorflow/models/blob/master/tutorials/rnn/ptb/ptb_word_lm.py
-  """
+    """The input data.
 
-def __init__(self, data, batch_size, num_steps, name=None):
-    self.batch_size = batch_size
-    self.num_steps = num_steps
-    self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
-    self.input_data, self.targets = ptb_reader.ptb_producer(
-        data, batch_size, num_steps, name=name)
+    Code sourced from https://github.com/tensorflow/models/blob/master/tutorials/rnn/ptb/ptb_word_lm.py
+    """
 
-raw_data = ptb_reader.ptb_raw_data(DATA_DIR)
-train_data, valid_data, test_data, _ = raw_data
-train_input = PTBInput(train_data, BATCH_SIZE, TIME_STEPS, name="TrainInput")
-
+    def __init__(self, data, batch_size, num_steps, name=None):
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.epoch_size = ((len(data) // batch_size) - 1) // num_steps
+        self.input_data, self.targets = ptb_reader.ptb_producer(
+            data, batch_size, num_steps, name=name)
 
 raw_data = ptb_reader.ptb_raw_data(DATA_DIR)
 train_data, valid_data, test_data, _ = raw_data
 train_input = PTBInput(train_data, BATCH_SIZE, TIME_STEPS, name="TrainInput")
+test_input = PTBInput(test_data, BATCH_SIZE, TIME_STEPS, name='TestInput')
 
 VOCAB_SIZE = 10000
 EMBEDDING_SIZE = 100
@@ -54,17 +51,17 @@ outputs, state = tf.nn.dynamic_rnn(lstm_cell, word_embeddings,
 
 logits = tf.layers.dense(outputs, VOCAB_SIZE)
 
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-2
 
-loss = tf.contrib.seq2seq.sequence_loss(
-    logits,
-    train_input.targets,
-    tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
-    average_across_timesteps=True,
-    average_across_batch=True)
+# loss = tf.contrib.seq2seq.sequence_loss(
+#     logits,
+#     train_input.targets,
+#     tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
+#     average_across_timesteps=True,
+#     average_across_batch=True)
 
 optimizer = tf.train.RMSPropOptimizer(LEARNING_RATE) # better than Adam for RNN networks
-train_op = optimizer.minimize(loss)
+train_op = optimizer.minimize(loss(train_input.targets))
 
 
 session = tf.Session()
@@ -75,9 +72,29 @@ coord = tf.train.Coordinator()
 threads = tf.train.start_queue_runners(sess=session, coord=coord)
 
 # retrieve some data to look at
-examples = session.run([train_input.input_data, train_input.targets])
+# examples = session.run([train_input.input_data, train_input.targets])
 # we can run the train op as usual
-_ = session.run(train_op)
+test_seq_loss = session.run([test_input.input_data, test_input.targets,
+    loss(test_input.targets)])
+
+for epoch in range(1000):
+    print('Epoch: ' + str(epoch))
+    train_losses = []
+    test_losses = []
+    for i in range(len(train_data) // BATCH_SIZE):
+        _, train_loss = session.run([train_op, loss(train_input.targets)])
+        train_losses.append(train_loss)
+    print('TRAIN LOSS: ' + str(np.average(train_losses)))
+    for i in range(len(test_data) // BATCH_SIZE):
+        test_loss = session.run(loss(test_input.targets))
+        test_losses.append(test_loss)
+    print('TEST LOSS: ' + str(np.average(test_losses)))
 
 
-
+def loss(targets):
+    return tf.contrib.seq2seq.sequence_loss(
+        logits,
+        targets,
+        tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
+        average_across_timesteps=True,
+        average_across_batch=True)
