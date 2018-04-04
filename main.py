8d0a2ab74ae1660c8edcccbec55907151003fb2a
asturtz2 <alexstur@gmail.com>
2018-04-03 21:18:04 -0500

    Add main and with statement

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import ptb_reader
 TIME_STEPS = 10
 BATCH_SIZE = 250
 
-tf.reset_default_graph()
 class PTBInput(object):
     """The input data.
 
@@ -31,73 +30,77 @@ def loss(targets):
         average_across_timesteps=True,
         average_across_batch=True)
 
-raw_data = ptb_reader.ptb_raw_data(DATA_DIR)
-train_data, valid_data, test_data, _ = raw_data
-train_input = PTBInput(train_data, BATCH_SIZE, TIME_STEPS, name="TrainInput")
-test_input = PTBInput(test_data, BATCH_SIZE, TIME_STEPS, name='TestInput')
+def main(argv):
+    tf.reset_default_graph()
+    raw_data = ptb_reader.ptb_raw_data(DATA_DIR)
+    train_data, valid_data, test_data, _ = raw_data
+    train_input = PTBInput(train_data, BATCH_SIZE, TIME_STEPS, name="TrainInput")
+    test_input = PTBInput(test_data, BATCH_SIZE, TIME_STEPS, name='TestInput')
 
-VOCAB_SIZE = 10000
-EMBEDDING_SIZE = 100
+    VOCAB_SIZE = 10000
+    EMBEDDING_SIZE = 100
 
-# setup input and embedding
-embedding_matrix = tf.get_variable('embedding_matrix', dtype=tf.float32, shape=[VOCAB_SIZE, EMBEDDING_SIZE], trainable=True)
-word_embeddings = tf.nn.embedding_lookup(embedding_matrix, train_input.input_data)
-print("The output of the word embedding: " + str(word_embeddings))
+    # setup input and embedding
+    embedding_matrix = tf.get_variable('embedding_matrix', dtype=tf.float32, shape=[VOCAB_SIZE, EMBEDDING_SIZE], trainable=True)
+    word_embeddings = tf.nn.embedding_lookup(embedding_matrix, train_input.input_data)
+    print("The output of the word embedding: " + str(word_embeddings))
 
-LSTM_SIZE = 200 # number of units in the LSTM layer, this number taken from a "small" language model
+    LSTM_SIZE = 200 # number of units in the LSTM layer, this number taken from a "small" language model
 
-lstm_cell = tf.contrib.rnn.BasicLSTMCell(LSTM_SIZE)
+    lstm_cell = tf.contrib.rnn.BasicLSTMCell(LSTM_SIZE)
 
-# Initial state of the LSTM memory.
-initial_state = lstm_cell.zero_state(BATCH_SIZE, tf.float32)
-print("Initial state of the LSTM: " + str(initial_state))
+    # Initial state of the LSTM memory.
+    initial_state = lstm_cell.zero_state(BATCH_SIZE, tf.float32)
+    print("Initial state of the LSTM: " + str(initial_state))
 
-# setup RNN
-outputs, state = tf.nn.dynamic_rnn(lstm_cell, word_embeddings,
-                                   initial_state=initial_state,
-                                   dtype=tf.float32)
+    # setup RNN
+    outputs, state = tf.nn.dynamic_rnn(lstm_cell, word_embeddings,
+                                       initial_state=initial_state,
+                                       dtype=tf.float32)
 
-logits = tf.layers.dense(outputs, VOCAB_SIZE)
+    logits = tf.layers.dense(outputs, VOCAB_SIZE)
 
-LEARNING_RATE = 1e-2
+    LEARNING_RATE = 1e-2
 
-# loss = tf.contrib.seq2seq.sequence_loss(
-#     logits,
-#     train_input.targets,
-#     tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
-#     average_across_timesteps=True,
-#     average_across_batch=True)
+    # loss = tf.contrib.seq2seq.sequence_loss(
+    #     logits,
+    #     train_input.targets,
+    #     tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
+    #     average_across_timesteps=True,
+    #     average_across_batch=True)
 
-optimizer = tf.train.RMSPropOptimizer(LEARNING_RATE) # better than Adam for RNN networks
-train_op = optimizer.minimize(loss(train_input.targets))
+    optimizer = tf.train.RMSPropOptimizer(LEARNING_RATE) # better than Adam for RNN networks
+    train_op = optimizer.minimize(loss(train_input.targets))
 
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
 
-session = tf.Session()
-session.run(tf.global_variables_initializer())
+        # start queue runners
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=session, coord=coord)
 
-# start queue runners
-coord = tf.train.Coordinator()
-threads = tf.train.start_queue_runners(sess=session, coord=coord)
+        # retrieve some data to look at
+        # examples = session.run([train_input.input_data, train_input.targets])
+        # we can run the train op as usual
+        test_seq_loss = session.run([test_input.input_data, test_input.targets,
+            loss(test_input.targets)])
 
-# retrieve some data to look at
-# examples = session.run([train_input.input_data, train_input.targets])
-# we can run the train op as usual
-test_seq_loss = session.run([test_input.input_data, test_input.targets,
-    loss(test_input.targets)])
+        for epoch in range(1000):
+            print('Epoch: ' + str(epoch))
+            train_losses = []
+            test_losses = []
+            loss_step = loss(train_input.targets)
+            for i in range(len(train_data) // BATCH_SIZE):
+                _, train_loss = session.run([train_op, loss_step])
+                train_losses.append(train_loss)
+                print('train_losses for step '+ str(i)+' is '+ str(train_loss))
+            print('TRAIN LOSS: ' + str(np.average(train_losses)))
+            loss_test_step = loss(test_input.targets)
+            for i in range(len(test_data) // BATCH_SIZE):
+                test_loss = session.run(loss_test_step)
+                test_losses.append(test_loss)
+            print('TEST LOSS: ' + str(np.average(test_losses)))
 
-for epoch in range(1000):
-    print('Epoch: ' + str(epoch))
-    train_losses = []
-    test_losses = []
-    loss_step = loss(train_input.targets)
-    for i in range(len(train_data) // BATCH_SIZE):
-        _, train_loss = session.run([train_op, loss_step])
-        train_losses.append(train_loss)
-        print('train_losses for step '+ str(i)+' is '+ str(train_loss))
-    print('TRAIN LOSS: ' + str(np.average(train_losses)))
-    loss_test_step = loss(test_input.targets)
-    for i in range(len(test_data) // BATCH_SIZE):
-        test_loss = session.run(loss_test_step)
-        test_losses.append(test_loss)
-    print('TEST LOSS: ' + str(np.average(test_losses)))
+if __name__ == '__main__':
+    tf.app.run()
 
